48c1d66215406b32f488432e9d2064d4e171b795
paskuvan <majopaskuvan@gmail.com>
2018-07-25 17:04:34 -0400

    scaffold area ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_201829) do
+ActiveRecord::Schema.define(version: 2018_07_25_210408) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
