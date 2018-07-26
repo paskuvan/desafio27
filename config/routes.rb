ec4ceafe7ae2266a37ba643ed6a92742e2e1f4f2
paskuvan <majopaskuvan@gmail.com>
2018-07-26 13:01:26 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'companies/index'
-  resources :areas
-  get 'categories/index'
-  resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	resources :categories do
+    resources :products, only: [:create, :destroy]
+  end
+  root 'categories#index'
 end
